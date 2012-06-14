ff025c43b529842908bab6c6d002f2073ad2a335
Emanuel Schmoczer <emanuel.schmoczer@schnibu.org>
2012-06-14 19:52:31 +0200

    Added ErlangBActivity

---
+++ ErlangCalculator/src/net/schmoczer/erlangcalculator/ErlangBActivity.java
@@ -0,0 +1,12 @@
+package net.schmoczer.erlangcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ErlangBActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
+}
