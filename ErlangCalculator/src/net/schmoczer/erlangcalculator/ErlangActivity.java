461f8377925666181ae091eef0287112a389d551
Emanuel Schmoczer <emanuel.schmoczer@schnibu.org>
2012-06-14 23:46:12 +0200

    ErlangActivity: implemented calculation.

--- ErlangCalculator/src/net/schmoczer/erlangcalculator/ErlangActivity.java
+++ ErlangCalculator/src/net/schmoczer/erlangcalculator/ErlangActivity.java
@@ -2,12 +2,51 @@ package net.schmoczer.erlangcalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ErlangActivity extends Activity {
+    private EditText editTextCalls;
+    private EditText editTextAvgCallTime;
+    private EditText editTextObservationPeriod;
+    private EditText editTextErlang;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.erlang);
+
+        editTextCalls = (EditText) findViewById(R.id.editTextCalls);
+        editTextAvgCallTime = (EditText) findViewById(R.id.editTextAvgCallTime);
+        editTextObservationPeriod = (EditText) findViewById(R.id.editTextObservationPeriod);
+        editTextErlang = (EditText) findViewById(R.id.editTextErlErlang);
+        Button buttonCalculate = (Button) findViewById(R.id.buttonErlCalculate);
+
+        buttonCalculate.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (editTextCalls.getEditableText().toString().trim().length() > 0
+                        && editTextAvgCallTime.getEditableText().toString().trim().length() > 0
+                        && editTextObservationPeriod.getEditableText().toString().trim().length() > 0) {
+                    double calls = Double.valueOf(editTextCalls.getText().toString().trim());
+                    double avgCallTime = Double.valueOf(editTextAvgCallTime.getText().toString()
+                            .trim());
+                    double observationPeriod = Double.valueOf(editTextObservationPeriod.getText()
+                            .toString().trim());
+                    if (calls > 0 && avgCallTime > 0 && observationPeriod > 0) {
+                        double erlang = calls * avgCallTime / observationPeriod;
+                        editTextErlang.setText(String.valueOf(erlang));
+                    }
+                } else {
+                    Toast.makeText(ErlangActivity.this, "TODO", Toast.LENGTH_LONG).show();// TODO
+                }
+
+            }
+        });
     }
 }
