26e75152672eb4275e697e68a307d20aac6ac9d2
Emanuel Schmoczer <emanuel.schmoczer@schnibu.org>
2012-06-14 21:41:26 +0200

    ErlangCalculatorActivity: added the 3 tabs.

--- ErlangCalculator/src/net/schmoczer/erlangcalculator/ErlangCalculatorActivity.java
+++ ErlangCalculator/src/net/schmoczer/erlangcalculator/ErlangCalculatorActivity.java
@@ -1,27 +1,47 @@
 package net.schmoczer.erlangcalculator;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.TabHost;
 
-public class ErlangCalculatorActivity extends Activity {
+public class ErlangCalculatorActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
-        
-        Button erlangActivityButton = (Button) findViewById(R.id.buttonErlangActivity);
-        erlangActivityButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(ErlangCalculatorActivity.this, ErlangActivity.class);
-				startActivity(intent);
-			}
-		});
+
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec tabSpec;
+        Intent intent;
+
+        // Erl-tab
+        intent = new Intent().setClass(this, ErlangActivity.class);
+        tabSpec = tabHost
+                .newTabSpec("erl")
+                .setIndicator(getResources().getString(R.string.erlang_text),
+                        getResources().getDrawable(R.drawable.ic_tab_erl)).setContent(intent);
+        tabHost.addTab(tabSpec);
+
+        // ErlangB-tab
+        intent = new Intent().setClass(this, ErlangBActivity.class);
+        tabSpec = tabHost
+                .newTabSpec("b")
+                .setIndicator(getResources().getString(R.string.erlangb_text),
+                        getResources().getDrawable(R.drawable.ic_tab_b)).setContent(intent);
+        tabHost.addTab(tabSpec);
+
+        // ErlangC-tab
+        intent = new Intent().setClass(this, ErlangCActivity.class);
+        tabSpec = tabHost
+                .newTabSpec("c")
+                .setIndicator(getResources().getString(R.string.erlangc_text),
+                        getResources().getDrawable(R.drawable.ic_tab_c)).setContent(intent);
+        tabHost.addTab(tabSpec);
+
+        tabHost.setCurrentTab(0);
+
     }
 }
