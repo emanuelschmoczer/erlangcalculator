81e36ee6ecd965e8ec5f057006d30b33681cb773
Emanuel Schmoczer <emanuel.schmoczer@schnibu.org>
2012-06-14 21:43:17 +0200

    ErlangActivity: set the layout.

--- ErlangCalculator/src/net/schmoczer/erlangcalculator/ErlangActivity.java
+++ ErlangCalculator/src/net/schmoczer/erlangcalculator/ErlangActivity.java
@@ -4,9 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class ErlangActivity extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.erlang);
+    }
 }
